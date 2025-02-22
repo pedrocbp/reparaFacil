6425b1ad952081365c22920e10ded0a3ceb21ad2
Pedro <pedromiguelboce@gmail.com>
2025-02-22 14:24:25 -0300

    feat: criacao do CreateCustomer

---
+++ src/controllers/CreateCustomerController.ts
@@ -0,0 +1,18 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { CreateCustomerService } from '../services/CreateCustomerService'; 
+
+class CreateCustomerController{
+    async handle(request: FastifyRequest, replay: FastifyReply){
+        
+        const { name, email } = request.body as { name: string, email: string };
+        console.log(name, email);
+        const customerService = new CreateCustomerService();
+
+        const customer = await customerService.execute({name, email});
+
+        replay.send(customer);
+
+    }
+}
+
+export { CreateCustomerController }
