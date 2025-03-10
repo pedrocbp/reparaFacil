ac6a16a198af3ea07f7ed96eb14b205c7ede28b4
Pedro <pedromiguelboce@gmail.com>
2025-03-09 23:23:48 -0300

    feat: add ApartamentoService for creating apartamentos with validation

---
+++ src/services/ApartamentoService.ts
@@ -0,0 +1,23 @@
+import prismaClient from "../prisma";
+
+interface CreateApartamentoProps {
+    name: string;
+    blocoId: number;
+}
+
+class ApartamentoService {
+    async create({ name, blocoId }: CreateApartamentoProps) {
+        if(!name || !blocoId){
+            throw new Error("Preencha todos os campos");
+        }
+
+        const apartamento = await prismaClient.apartamentos.create({
+            data: { 
+                name,
+                blocoId
+            }
+        })
+        return apartamento;
+    }
+}
+export { ApartamentoService };
