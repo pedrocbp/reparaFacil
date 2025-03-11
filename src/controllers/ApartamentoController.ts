a91a8916060d23980dcb8c838c6a0ff38662abac
Pedro <pedromiguelboce@gmail.com>
2025-03-10 21:32:11 -0300

    feat: add update em ApartamentoController

--- src/controllers/ApartamentoController.ts
+++ src/controllers/ApartamentoController.ts
@@ -18,5 +18,15 @@ class ApartamentoController {
 
         reply.send(apartamento);
     }
+
+    async update(request: FastifyRequest, reply: FastifyReply){
+        const {id} = request.query as {id: number}
+        const {name, blocoId, status} = request.body as {name?: string, blocoId?: number, status?: boolean}
+
+
+        const apartamento = await new ApartamentoService().update({id, name, blocoId, status});
+
+        reply.send(apartamento)
+    }
 }
 export { ApartamentoController };
